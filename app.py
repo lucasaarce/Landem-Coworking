4a0dd9bc363112b73738913407c662fbfa9ca39a
lucasaarce <lucasarce180105@gmail.com>
2025-05-21 21:38:51 +0200

    Third Load (Final Prove)

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
+import smtplib
+from email.mime.text import MIMEText
+
 # Ruta absoluta al frontend
 FRONTEND_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'frontend'))
 
@@ -24,28 +27,56 @@ def assets_static(filename):
     return send_from_directory(os.path.join(FRONTEND_PATH, 'assets'), filename)
 
 load_dotenv()
-# Conexión a PostgreSQL
-try:
-    conn = psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        host=os.getenv("DB_HOST"),
-        port='5432'
-    )
-    print("✅ Conectado a PostgreSQL")
-except Exception as e:
-    print("❌ Error al conectar a PostgreSQL:", e)
-    conn = None
+
+# try:
+#     conn = psycopg2.connect(
+#         dbname=os.getenv("DB_NAME"),
+#         user=os.getenv("DB_USER"),
+#         password=os.getenv("DB_PASS"),
+#         host=os.getenv("DB_HOST"),
+#         port='5432'
+#     )
+#     print("✅ Conectado a PostgreSQL")
+# except Exception as e:
+#     print("❌ Error al conectar a PostgreSQL:", e)
+#     conn = None
+
+def enviar_correo(email, telefono, empresa, plan, personas, mensaje):
+    try:
+        cuerpo = f"""
+        Tienes una nueva solicitud en el espacio de coworking de {email}:  
+
+        Teléfono: {telefono}
+        Empresa: {empresa}
+        Plan: {plan}
+        Personas: {personas}
+        
+        {mensaje}
+        """
+
+        msg = MIMEText(cuerpo)
+        msg['Subject'] = 'Nuevo formulario de contacto'
+        msg['From'] = os.getenv("EMAIL_USER")
+        msg['To'] = os.getenv("EMAIL_TO")
+
+        with smtplib.SMTP(os.getenv("EMAIL_HOST"), int(os.getenv("EMAIL_PORT"))) as server:
+            server.starttls()
+            server.login(os.getenv("EMAIL_USER"), os.getenv("EMAIL_PASS"))
+            server.send_message(msg)
+        print("✅ Correo enviado")
+    except Exception as e:
+        print("❌ Error al enviar correo:", e)
+
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    idioma = request.form.get("idioma", "frontend/index")
+    return redirect(f'/{idioma}.html')
 
 @app.route('/enviar', methods=['POST'])
 def enviar():
-    if conn is None:
-        return "Error de conexión a base de datos", 500
+    # if conn is None:
+    #     return "Error de conexión a base de datos", 500
 
     try:
         email = request.form['email']
@@ -59,20 +90,21 @@ def enviar():
         print("📨 Datos recibidos:")
         print(email, telefono, empresa, plan, personas, mensaje, privacidad)
 
-        cur = conn.cursor()
-        cur.execute("""
-            INSERT INTO formulario_contacto 
-            (email, telefono, empresa, plan, personas, mensaje, privacidad)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
-        """, (email, telefono, empresa, plan, personas, mensaje, privacidad))
-        conn.commit()
-        cur.close()
-
-        return redirect('/')
+        # cur = conn.cursor()
+        # cur.execute("""
+        #     INSERT INTO formulario_contacto 
+        #     (email, telefono, empresa, plan, personas, mensaje, privacidad)
+        #     VALUES (%s, %s, %s, %s, %s, %s, %s)
+        # """, (email, telefono, empresa, plan, personas, mensaje, privacidad))
+        # conn.commit()
+        # cur.close()
+        enviar_correo(email, telefono, empresa, plan, personas, mensaje) #correo
+        idioma = request.form.get("idioma", "frontend/index")
+        return redirect(f'/frontend/redirect-{idioma}.html')
+        # return redirect('frontend/requested.html')
     except Exception as e:
         print("❌ Error al insertar en la base de datos:", e)
         return "Error al procesar el formulario", 500
 
-# 🚨 Asegúrate de tener esto al final del archivo
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
